f3b31c5eb755e5b07902277fdd84dfb166cf1319
Administrator <miti1383@gmail.com>
2024-11-23 23:22:40 +0330

    first commit

---
+++ game2048/src/main/java/org/example/game2048/HelloController.java
@@ -0,0 +1,55 @@
+package org.example.game2048;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    public Label l00;
+    @FXML
+    public Label l01;
+    @FXML
+    public Label l02;
+    @FXML
+    public Label l03;
+    @FXML
+    public Label l10;
+    @FXML
+    public Label l11;
+    @FXML
+    public Label l12;
+    @FXML
+    public Label l13;
+    @FXML
+    public Label l20;
+    @FXML
+    public Label l21;
+    @FXML
+    public Label l22;
+    @FXML
+    public Label l23;
+    @FXML
+    public Label l30;
+    @FXML
+    public Label l31;
+    @FXML
+    public Label l32;
+    @FXML
+    public Label l33;
+
+
+
+    public void move_up(){
+
+    }
+    public void move_down(){
+
+    }
+    public void move_right(){
+
+    }
+    public void move_left(){
+
+    }
+}
